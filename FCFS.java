d55c738f8800d95255cf4e740c7c54219ae43b8f
Mohammad <dr.fmfm2004@gmail.com>
2025-04-23 16:42:52 +0300

    add waiting and turnaround time

--- FCFS.java
+++ FCFS.java
@@ -5,12 +5,25 @@ public class FCFS {
   public static void run( Queue<PCB> readyQueue ){
 
     int currentTime = 0;
+    int count = 0;
+    int total_Waiting_Time = 0;
+    int AVG_WaitingTime = 0;
+    int total_Turnaround_Time = 0;
+    int AVG_turnaroundTime = 0;
     System.out.println("Gantt Chart: ");
 
     while(!readyQueue.isEmpty()){
       PCB p = readyQueue.poll();
+      
+      count++;
       int Enter = currentTime;
       p.waitingTime = currentTime;
+      
+      total_Waiting_Time = total_Waiting_Time + p.waitingTime;
+      AVG_WaitingTime = total_Waiting_Time/count; 
+      total_Turnaround_Time = total_Turnaround_Time + currentTime;
+      AVG_turnaroundTime = total_Turnaround_Time/count;
+
       currentTime = currentTime + p.burstTime;
       p.turtnaroundTime = currentTime;
       try {
