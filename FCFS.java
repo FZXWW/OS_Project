d85062b09876baa2290dc25dac311796b12d208e
Mohammad <dr.fmfm2004@gmail.com>
2025-04-24 10:12:26 +0300

    edit in all the FCFS (TT and WT) also the Gantt Chart output

--- FCFS.java
+++ FCFS.java
@@ -1,6 +1,7 @@
 import java.util.Queue;
 
 public class FCFS {
+  
 
   public static void run( Queue<PCB> readyQueue ){
 
@@ -11,32 +12,42 @@ public class FCFS {
     int total_Turnaround_Time = 0;
     int AVG_turnaroundTime = 0;
     System.out.println("Gantt Chart: ");
+    System.out.println("+------------------+----------------+--------------+-------------------+----------------+");
+    System.out.println("|    Process ID    |     Start      |      End     | Turnaround Time   | Waiting Time   |");
+    System.out.println("+------------------+----------------+--------------+-------------------+----------------+");
+    
 
     while(!readyQueue.isEmpty()){
       PCB p = readyQueue.poll();
       
       count++;
       int Enter = currentTime;
-      p.waitingTime = currentTime;
       
+      p.waitingTime = currentTime;
       total_Waiting_Time = total_Waiting_Time + p.waitingTime;
-      AVG_WaitingTime = total_Waiting_Time/count; 
-      total_Turnaround_Time = total_Turnaround_Time + currentTime;
-      AVG_turnaroundTime = total_Turnaround_Time/count;
-
+      
       currentTime = currentTime + p.burstTime;
-      p.turtnaroundTime = currentTime;
+      p.turtnaroundTime = p.burstTime + p.waitingTime;
+      total_Turnaround_Time = total_Turnaround_Time + (p.turtnaroundTime );
+      
       try {
         Thread.sleep(1000); // 1 second delay (adjust as needed)
-        System.out.println("      P" + p.id + "      |");
-        System.out.print(Enter +"          " +p.turtnaroundTime);
+        System.out.printf("| %16d | %14d | %12d | %17d | %14d |\n",p.id,Enter, p.turtnaroundTime,p.turtnaroundTime, p.waitingTime );
+        System.out.println("+------------------+----------------+--------------+-------------------+----------------+");
 
-    } catch (InterruptedException e) {
+       } catch (InterruptedException e) {
         e.printStackTrace();
+        }
     }
-    }
+    AVG_turnaroundTime = total_Turnaround_Time/count;
+    AVG_WaitingTime = total_Waiting_Time/count; 
     System.out.println();
+    System.out.println("The average waiting time is: " + AVG_WaitingTime);
+    System.out.println("The average turnaround time is: " + AVG_turnaroundTime);
+
 
   }
 
 }
+
+
